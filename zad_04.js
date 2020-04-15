f31285dafcb83497b1cd14c11ed310808e483f80
maciejszczesny <szczesnymcj@gmail.com>
2020-04-15 14:13:13 +0200

    zadanie 4 lab01

---
+++ zad_04.js
@@ -0,0 +1,15 @@
+const rmNums = (arr, arg1, ...args) => {
+    arr = arr.filter((item) => {
+        return item !== arg1
+    })
+
+    args.forEach((arg) => {
+        arr = arr.filter((item) => {
+            return item !== arg
+        })
+    })
+
+    return arr
+}
+
+console.log(rmNums([1,2,3,4,5,6], 1, 3, 4))
