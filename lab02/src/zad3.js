ddb575b9b5f34baa6dc3294c809dc8f4b90d9d11
maciejszczesny <szczesnymcj@gmail.com>
2020-04-22 13:22:11 +0200

    zadanie 3 pkt 3 i 4(nie całe)

--- lab02/src/zad3.js
+++ lab02/src/zad3.js
@@ -4,8 +4,16 @@ import lp3 from './toplist.js';
 import _ from 'lodash';
 
 const queenSongs = _.filter(lp3, { 'author': 'Queen' });
-const PFSongs = _.filter(lp3, { 'author': 'Pink Floyd' })
+const PFSongs = _.filter(lp3, { 'author': 'Pink Floyd' });
 const mostVolatilePF = _.filter(PFSongs, (o) => { return o.change > 10; });
+const sortedByVolatility = _.sortBy(lp3, 'change');
+const dropN = (arr, n) => {
+    return _.dropRight(arr, n);
+};
+const first = _.take(lp3, 1);
 
-console.log(queenSongs)
-console.log(mostVolatilePF)
+
+console.log(queenSongs);
+console.log(mostVolatilePF);
+console.log(dropN(sortedByVolatility, 10));
+console.log(first);
