79e822626ec22f73c988797c8e72549075322e04
maciejszczesny <szczesnymcj@gmail.com>
2020-04-29 15:20:42 +0200

    zadanie 05 zaczęte

---
+++ lab03/src/zad_05.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const arr = [
+    { key: 0, x: [4, 5, 6], y: [1, 2, 3, 4] },
+    { key: 0, x: [1], y: [] }
+];
+
+arr.reduce((acc, cur) => {
+    return cur.forEach((el) => typeof curLocal === Array ? el.length )
+}, 0)
