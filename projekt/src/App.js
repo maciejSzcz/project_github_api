a0e44d0f19a062d75b23e97b8b2dc25a8f51aafd
maciejszczesny <szczesnymcj@gmail.com>
2020-06-02 23:37:54 +0200

    app now renders most popular repos on github

--- projekt/src/App.js
+++ projekt/src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+function App(props) {
+  const [popularProjects, setPopularProjects] = useState([])
+
+
+
+  useEffect(() => {
+    axios
+      .get('https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc')
+      .then(response => {
+        setPopularProjects(response.data.items)
+      })
+      .catch(err => console.error(err))
+
+  }, [])
+
+
   return (
     <div className="App">
-      <p>projekt github</p>
+      <div className="Wrapper">
+        <p className="Name">Github Trending</p>
+        <p className="RepoNumber">Most popular repositiories now are </p>
+        <ul>
+          {
+            popularProjects
+              .map(project => (
+                <li key={project.name}>{project.name}</li>
+              ))
+          }
+        </ul>
+      </div>
     </div>
   );
 }
